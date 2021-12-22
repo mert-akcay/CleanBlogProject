73fed3d3a39818b120d2bcb99704d1d409f997f4
Mert Akcay <akcaymert603@gmail.com>
2021-12-23 02:02:50 +0300

    Static files added and view engine setup

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//STATIC FILES
+app.use(express.static('public'));
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//ROUTES
 app.get('/', (req, res) => {
-  let data = {
-    id: 1,
-    title: 'Blog Title',
-    description: 'Blog Description',
-  };
-  res.send(data);
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('index')
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
 });
 
 const port = 3000;
