ccd7a1c8e3bea2ae0a3be7bb2e4c35f130a724ab
Mert Akcay <akcaymert603@gmail.com>
2021-12-24 22:02:21 +0300

    Improvements and MVC Structure

---
+++ controllers/pageControllers.js
@@ -0,0 +1,7 @@
+exports.addPost = (req, res) => {
+  res.render('add_post');
+};
+
+exports.about = (req, res) => {
+    res.render('about');
+  };
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const methodOverride = require('method-override');
 const Post = require('./models/Post');
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageControllers')
+
 
 const app = express();
 
@@ -15,38 +19,23 @@ mongoose.connect('mongodb://localhost/cleanblog-test-db', {
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method',{
+  methods:['GET','POST']
+}));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post,
-  });
-});
+//ROUTES
+app.get('/', postController.getAllPosts());
+app.get('/posts/:id', postController.getPost);
+app.get('/posts/edit/:id', postController.getPost2Edit);
+app.get('/add_post', pageController.addPost);
+app.get('/about', pageController.about);
+app.post('/posts', postController.addPost);
+app.put('/posts/:id', postController.editPost);
+app.delete('/posts/:id',postController.deletePost);
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {
-    posts,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('index');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.post('/posts', async (req, res) => {
-  console.log(req.body);
-  await Post.create(req.body);
-  res.redirect('/');
-});
 
 const port = 3001;
 app.listen(port, () => {
